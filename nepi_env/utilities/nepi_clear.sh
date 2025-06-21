5457f3ebb74f7dcd89c3cc09a740b59d1e9597b5
Jason Seawall <jseawall@numurus.com>
2025-06-21 12:36:43 -0700

    *3p2p0RCK*

---
+++ nepi_env/utilities/nepi_clear.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+##
+## Copyright (c) 2024 Numurus, LLC <https://www.numurus.com>.
+##
+## This file is part of nepi-engine
+## (see https://github.com/nepi-engine).
+##
+## License: 3-clause BSD, see https://opensource.org/licenses/BSD-3-Clause
+##
+
+
+# This script deletes all nepi folders/files in the nepi system
+cd /opt/nepi/ros
+sudo rm -r nepi_* ./*
