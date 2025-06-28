c3b596cc1abfb90bf1c7caea29ae88891e41f17d
AsherSeawall <aseawall@numurus.com>
2025-06-28 11:19:49 -0700

    Serial Updates

--- nepi_sdk/src/nepi_sdk/nepi_serial.py
+++ nepi_sdk/src/nepi_sdk/nepi_serial.py
@@ -15,7 +15,8 @@
 import time
 import usb
 import copy
-from serial
+import serial
+from serial.tools import list_ports
 
 from nepi_sdk import nepi_sdk
 from nepi_sdk import nepi_utils
@@ -32,7 +33,7 @@ logger = Logger(log_name = log_name)
 STANDARD_BUAD_RATES = [110, 150, 300, 1200, 2400, 4800, 9600, 19200, 38400, 57600, 115200, 230400, 460800, 921600]
 
 SERIAL_DEVICE_DICT = dict(    
-                        serial_port = "",
+                        port = "",
                         baudrate = 0,
                         addr_str = "",
                         manf_str = "None",
@@ -40,16 +41,26 @@ SERIAL_DEVICE_DICT = dict(
                         product_id = 0,
                         connected = False
                    )
-                  
+
+
+def get_serial_ports_list():
+    ports_list = []
+    ports = list_ports.comports()
+    for loc, desc, hwid in sorted(ports):
+        ports_list.append(loc)
+    return ports_list
+
+
+
 # Function for getting info for all available (non-used) serial ports
 def get_serial_ports_dict_list():
   port_dicts = dict()
   usb_ports = usb.core.find(find_all=True)
-  ports = serial.tools.list_port.comports()
-  for serial_port in sorted(ports):
+  ports = list_ports.comports() #get_serial_ports_list()
+  for port in sorted(ports):
     device_dict = SERIAL_DEVICE_DICT
-    device_dict = get_serial_device_dict(serial_port)
-    port_dicts[serial_port.port] = device_dict
+    device_dict = get_serial_device_dict(port)
+    port_dicts[port.port] = device_dict
   return port_dicts
   
 
@@ -68,14 +79,16 @@ def get_serial_device_dict(serial_port):
                 product_id = usb_port.idProduct
                 break
         device_dict["product_id"] = product_id
-  return device_dict
+    except Exception as e:
+        logger.log_debug("Failed to get serial device info: " + str(e))
+    return device_dict
 
 
 
 # Function for checking if serial_port is available
 def check_for_serial_port(port_str):
     success = False
-    serial_port = serial.tools.list_port.comports()
+    serial_port = list_ports.comports()
     for loc, desc, hwid in sorted(serial_port):
       if loc == port_str:
         success = True
@@ -90,9 +103,9 @@ def send_serial_message(serial_port,
     # Add carriage return and line feed if needed
     ser_str= message_str
     if include_cr == True:
-        ser_str = (ser_msg + '\r')
+        ser_str = (ser_str + '\r')
     if include_lf == True:
-        ser_str = (ser_msg + '\n')
+        ser_str = (ser_str + '\n')
     ################################################  
     # Send Serial String
     logger.log_debug("")
@@ -107,87 +120,88 @@ def send_serial_message(serial_port,
         bs = serial_port.readline()
         response = bs.decode()
     except Exception as e:
-        logger.log_degub("Got a serial read/write error: " + str(e))
+        logger.log_debug("Got a serial read/write error: " + str(e))
     return response
 
 
 # Function for serial send/receive check
-def check_for_device_on_serial_port(port_str, 
+def check_serial_port_by_product_id(port_str, 
                                     baudrate = 9600, 
-                                    message_str = '', 
-                                    response_test_function = None, 
-                                    include_cr = True, 
-                                    include_lf = True,
-                                    wait_time = 0.010):
+                                    product_id = 0,
+                                    wait_time = 0.010,
+                                    verbose = False):
+    valid = False
     device_dict = None
+    if verbose == True:
+            msg = ("Starting serial device product id check " + str(port_str))
+            print(msg)
+            logger.log_info(msg)
     try:
         # Try and open serial port
         serial_port = serial.Serial(port_str,baudrate,timeout = 1)
     except Exception as e:
-        logger.log_info("Unable to open serial port " + path_str + " with baudrate: " + str(baudrate) + " " + str(e))
+        msg = ("Unable to open serial port " + str(port_str) + " with baudrate: " + str(baudrate) + " " + str(e))
+        print(msg)
+        logger.log_info(msg)
         return device_dict
-    # Add carriage return and line feed if needed
-    response = send_serial_message(serial_port, 
-                            message_str = message_str, 
-                            include_cr = include_cr, 
-                            include_lf = include_lf, 
-                            wait_time = wait_time)
-    if response is not None:
-
-        
-        if response_test_function is not None:
-            try:
-                valid = response_test_function(message_str,response)
-            except:
-                pass
-        else:
-            valid = (response == message_str)
-        if response == response_str:
-            device_dict = get_serial_device_dict(serial_port)
+    dd = get_serial_device_dict(serial_port)
+    if device_dict is not None:
+        if product_id == dd['product_id']:
+            valid = True
+    if valid:
+        device_dict = dd
     # Clean up the serial port
     serial_port.close()
     return device_dict
-
-
 # Function for tserial send/receive check for list of ports, addrs, baud_rates 
-def check_for_device_on_serial_ports(
-                    message_start_str = '', 
-                    message_addr_str_list = [''], 
-                    message_stop_str = '',
-                    response_test_function = None, # Will use echo of command if None
+
+def check_serial_ports_by_product_id(
+                    port_str_list = None,
                     buadrate_list = STANDARD_BUAD_RATES,
+                    product_id = 0,
                     include_cr = True, 
                     include_lf = True,
                     wait_time = 0.010,
                     verbose = False):
 
     device_dict = None
-    port_str_list = serial.tools.list_port.comports()
-    for port_str in port_str_list
-        for baudrate in buadrate_list:
-            for addr_str in message_addr_str_list:
-                message_str = message_start_str + addr_str + message_stop_str
+    if port_str_list is None:
+        port_str_list = get_serial_ports_list()
 
+    for port_str in port_str_list:
+        for baudrate in buadrate_list:
                 if verbose == True:
-                    msg = ("Checking for serial device " + port_str + " baudrate: " + str(baudrate) + \
-                    " message: " + str(message_str))
+                    msg = ("Checking for serial device " + str(port_str) + " product_id: " + str(product_id))
                     print(msg)
                     logger.log_info(msg)
-                device_dict = check_for_device_on_serial_port(
+                device_dict = check_serial_port_by_product_id(
                                                         port_str, 
                                                         baudrate = baudrate, 
-                                                        message_str = message_str,
-                                                        response_test_function = response_test_function, 
+                                                        product_id = product_id, 
                                                         include_cr = include_cr, 
-                                                        include_lf = include_lf)
+                                                        include_lf = include_lf,
+                                                        verbose = verbose)
                 if device_dict is not None:
                     device_dict['addr_str'] = addr_str
                     if verbose == True:
                         msg = ("Found Device: " + str(device_dict))
                         print(msg)
                         logger.log_info(msg)
-                    break
+                    return device_dict
     return device_dict
+
+
+
+
+    
+def update_msg_to_length(message, length = None, length_prefix = '', length_suffix = ''):
+    msg_str = message
+    if length == None or lenght == 0 or len(message) >= length or (length_prefix == '' and \
+                    length_suffix == '') or (length_prefix != '' and length_suffix != ''):
+        return msg_str
+    for i in range(length):
+        data_str = length_prefix + data_str + length_suffix
+
 
 def create_serial_port_addrs_list(start_str='1', stop_str='2', length = None, length_prefix = '', length_suffix = ''):
     addr_list = []
@@ -210,16 +224,16 @@ def create_serial_port_addrs_list(start_str='1', stop_str='2', length = None, le
             if start_str.islower() and stop_str.islower():
                 try:
                     letters = nepi_utils.get_lowercase_letters()
-                    start_ind = letters.index(start_addr)
-                    stop_ind = letters.index(stop_addr)
+                    start_ind = letters.index(start_str)
+                    stop_ind = letters.index(stop_str)
                     addr_list = list(letters[start_ind:stop_ind + 1])
                 except:
                     pass
             if start_str.isupper() and stop_str.isupper():
                 try:
                     letters = nepi_utils.get_uppercase_letters()
-                    start_ind = letters.index(start_addr)
-                    stop_ind = letters.index(stop_addr)
+                    start_ind = letters.index(start_str)
+                    stop_ind = letters.index(stop_str)
                     addr_list = list(letters[start_ind:stop_ind + 1])
                 except:
                     pass
@@ -228,7 +242,156 @@ def create_serial_port_addrs_list(start_str='1', stop_str='2', length = None, le
     addr_list_adj = []
     for addr in addr_list:
         adj_addr = update_msg_to_length(addr, length = length, length_prefix = length_prefix, length_suffix = length_suffix)
-        addr_list_adj.append()
+        addr_list_adj.append(adj_addr)
+    return addr_list_adj
+
+        
+
+
+
+# Function for serial send/receive check
+def check_serial_port_by_message(
+                                port_str, 
+                                baudrate = 9600, 
+                                message_str = '', 
+                                response_test_function = None, 
+                                include_cr = True, 
+                                include_lf = True,
+                                wait_time = 0.010,
+                                verbose = False
+                                ):
+    valid = False
+    device_dict = None
+    if verbose == True:
+            msg = ("Starting serial device check " + str(port_str))
+            print(msg)
+            logger.log_info(msg)
+    try:
+        # Try and open serial port
+        serial_port = serial.Serial(port_str,baudrate,timeout = 1)
+    except Exception as e:
+        msg = ("Unable to open serial port " + str(port_str) + " with baudrate: " + str(baudrate) + " " + str(e))
+        print(msg)
+        logger.log_info(msg)
+        return device_dict
+    # Add carriage return and line feed if needed
+    if verbose == True:
+        msg = ("Sending serial msg " + str(message_str))
+        print(msg)
+        logger.log_info(msg)
+    response = send_serial_message(serial_port, 
+                            message_str = message_str, 
+                            include_cr = include_cr, 
+                            include_lf = include_lf, 
+                            wait_time = wait_time)
+    if response is not None:
+        if verbose == True:
+                msg = ("Got serial response " + str(response))
+                print(msg)
+                logger.log_info(msg)
+        valid = False
+        if response_test_function is not None:
+            try:
+                valid = response_test_function(message_str,response)
+            except:
+                pass
+        else:
+            valid = (response == message_str)
+        if valid:
+            device_dict = get_serial_device_dict(serial_port)
+    # Clean up the serial port
+    serial_port.close()
+    return device_dict
+
+
+
+# Function for tserial send/receive check for list of ports, addrs, baud_rates 
+def check_serial_ports_by_message(
+                    port_str_list = None,
+                    message_start_str = '', 
+                    message_addr_str_list = [''], 
+                    message_stop_str = '',
+                    response_test_function = None, # Will use echo of command if None
+                    buadrate_list = STANDARD_BUAD_RATES,
+                    include_cr = True, 
+                    include_lf = True,
+                    wait_time = 0.010,
+                    verbose = False):
+
+    device_dict = None
+    print("Got ports: " + str(port_str_list))
+    if port_str_list is None:
+        port_str_list = get_serial_ports_list()
+        print("Found ports: " + str(port_str_list))
+    print("Searching ports: " + str(port_str_list))
+
+    for port_str in port_str_list:
+        for baudrate in buadrate_list:
+            for addr_str in message_addr_str_list:
+                message_str = message_start_str + addr_str + message_stop_str
+
+                if verbose == True:
+                    msg = ("Checking for serial device " + str(port_str) + " baudrate: " + str(baudrate) + " message: " + str(message_str))
+                    print(msg)
+                    logger.log_info(msg)
+                device_dict = check_serial_port_by_message(
+                                                        port_str, 
+                                                        baudrate = baudrate, 
+                                                        message_str = message_str,
+                                                        response_test_function = response_test_function, 
+                                                        include_cr = include_cr, 
+                                                        include_lf = include_lf,
+                                                        verbose = verbose)
+                if device_dict is not None:
+                    device_dict['addr_str'] = addr_str
+                    if verbose == True:
+                        msg = ("Found Device: " + str(device_dict))
+                        print(msg)
+                        logger.log_info(msg)
+                    return device_dict
+    return device_dict
+
+
+def create_serial_port_addrs_list(start_str='1', stop_str='2', length = None, length_prefix = '', length_suffix = ''):
+    addr_list = []
+
+    # First Check if Int
+    try:
+      start_addr = int(start_str)
+      stop_addr = int(stop_str)
+      addr_range = stop_addr - start_addr
+      if addr_range > 0:
+        self.addr_list = list(range(start_addr,stop_addr+1))
+      else:
+        self.addr_list = [start_addr]    
+    except:
+        pass
+
+    # Check if letters with matching cases
+    if len(start_str) == 1 and len(stop_str) == 1:
+        if start_str.isalpha() and stop_str.isalpha():
+            if start_str.islower() and stop_str.islower():
+                try:
+                    letters = nepi_utils.get_lowercase_letters()
+                    start_ind = letters.index(start_str)
+                    stop_ind = letters.index(stop_str)
+                    addr_list = list(letters[start_ind:stop_ind + 1])
+                except:
+                    pass
+            if start_str.isupper() and stop_str.isupper():
+                try:
+                    letters = nepi_utils.get_uppercase_letters()
+                    start_ind = letters.index(start_str)
+                    stop_ind = letters.index(stop_str)
+                    addr_list = list(letters[start_ind:stop_ind + 1])
+                except:
+                    pass
+
+    # Adjust for length if needed
+    addr_list_adj = []
+    for addr in addr_list:
+        adj_addr = update_msg_to_length(addr, length = length, length_prefix = length_prefix, length_suffix = length_suffix)
+        addr_list_adj.append(adj_addr)
     return addr_list_adj
 
         
@@ -238,7 +401,7 @@ def create_serial_port_addrs_list(start_str='1', stop_str='2', length = None, le
 def update_msg_to_length(message, length = None, length_prefix = '', length_suffix = ''):
     msg_str = message
     if length == None or lenght == 0 or len(message) >= length or (length_prefix == '' and \
-                    length_suffix == '') or or (length_prefix != '' and length_suffix != ''):
+                    length_suffix == '') or (length_prefix != '' and length_suffix != ''):
         return msg_str
     for i in range(length):
         data_str = length_prefix + data_str + length_suffix
