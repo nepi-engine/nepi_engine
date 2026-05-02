51f889202de54953a2e3383b57164345cdc95fbd
jseawall@numurus.com <jseawall@numurus.com>
2026-05-02 13:37:23 -0700

    **

--- nepi_sdk/src/nepi_sdk/nepi_aifs.py
+++ nepi_sdk/src/nepi_sdk/nepi_aifs.py
@@ -163,7 +163,7 @@ def unimportAIFClass(module_name):
 
 def loadModelsDict(framework_name, pkg_name, models_folder_path):
     models_dict = dict()
-    base_path = os.path.basename(models_folder_path)
+    base_path = os.path.dirname(models_folder_path)
     if os.path.exists(base_path) == False:
         logger.log_warn("Failed to find models folder: " + base_path)
         return models_dict  
